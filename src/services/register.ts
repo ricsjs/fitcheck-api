c5085e9d6cdb3931ddccf853a881c3a319166d85
ricsjs <ricfilho0007@gmail.com>
2024-02-22 15:11:45 -0300

    feat: add global error handling

--- src/services/register.ts
+++ src/services/register.ts
@@ -1,6 +1,7 @@
 import { prisma } from "@/lib/prisma"
 import { UsersRepository } from "@/repositories/users-repository"
 import { hash } from "bcryptjs"
+import { UserAlreadyExistsError } from "./errors/user-already-exists-error"
 
 interface RegisterServiceRequest {
     name: string
@@ -22,7 +23,7 @@ export class RegisterService {
         const userWithSameEmail = await this.usersRepository.findByEmail(email)
     
         if (userWithSameEmail) {
-            throw new Error('E-mail already exists')
+            throw new UserAlreadyExistsError()
         }
     
         await this.usersRepository.create({
