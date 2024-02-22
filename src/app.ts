19ee1fafa5a494d46ed708b95f894999b4001d3a
ricsjs <ricfilho0007@gmail.com>
2024-02-22 11:11:36 -0300

    feat: create user

--- src/app.ts
+++ src/app.ts
@@ -1,13 +1,26 @@
 import fastify from "fastify"
-import { PrismaClient } from "@prisma/client"
+import { z } from 'zod'
+import { prisma } from "./lib/prisma"
 
 export const app = fastify()
 
-const prisma = new PrismaClient()
 
-prisma.user.create({
-    data: {
-        name: 'Ricardo',
-        email: 'ricfilho0007@gmail.com'
-    }
+app.post('/users', async (request, reply) => {
+    const registerBodySchema = z.object({
+        name: z.string(),
+        email: z.string().email(),
+        password: z.string().min(6),
+    })
+
+    const { name, email, password } = registerBodySchema.parse(request.body)
+
+    await prisma.user.create({
+        data: {
+            name,
+            email,
+            password_hash: password
+        }
+    })
+
+    return reply.status(201).send()
 })
