66f03921e060c12c0382691ec5a10a06e86ad02d
ricsjs <ricfilho0007@gmail.com>
2024-02-22 11:41:29 -0300

    feat: separation of http creation and calling functionality

--- src/http/controllers/register.ts
+++ src/http/controllers/register.ts
@@ -1,7 +1,6 @@
 import { FastifyRequest, FastifyReply } from 'fastify'
-import { hash } from 'bcryptjs'
-import { prisma } from "@/lib/prisma"
 import { z } from "zod"
+import { registerService } from '@/services/register'
 
 export async function register(request: FastifyRequest, reply: FastifyReply) {
     const registerBodySchema = z.object({
@@ -12,25 +11,16 @@ export async function register(request: FastifyRequest, reply: FastifyReply) {
 
     const { name, email, password } = registerBodySchema.parse(request.body)
 
-    const password_hash = await hash(password, 6)
-
-    const userWithSameEmail = await prisma.user.findUnique({
-        where: {
-           email 
-        }
-    })
-
-    if (userWithSameEmail) {
-        return reply.status(409).send()
-    }
-
-    await prisma.user.create({
-        data: {
+    try {
+        await registerService({
             name,
             email,
-            password_hash,
-        }
-    })
+            password
+        })
+
+    } catch (error) {
+        return reply.status(409).send()
+    }
 
     return reply.status(201).send()
 }
---
+++ src/services/register.ts
@@ -0,0 +1,32 @@
+import { prisma } from "@/lib/prisma"
+import { hash } from "bcryptjs"
+
+interface RegisterServiceRequest {
+    name: string
+    email: string
+    password: string
+}
+
+export async function registerService({
+    name, email, password
+} : RegisterServiceRequest) {
+    const password_hash = await hash(password, 6)
+
+    const userWithSameEmail = await prisma.user.findUnique({
+        where: {
+           email 
+        }
+    })
+
+    if (userWithSameEmail) {
+        throw new Error('E-mail already exists')
+    }
+
+    await prisma.user.create({
+        data: {
+            name,
+            email,
+            password_hash,
+        }
+    })
+}
