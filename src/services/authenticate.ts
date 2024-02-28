ad754704452ccbde34f6d04816502b71165113f4
ricsjs <ricfilho0007@gmail.com>
2024-02-28 20:22:55 -0300

    feat: authenticate user service

---
+++ src/services/authenticate.ts
@@ -0,0 +1,37 @@
+import { UsersRepository } from "@/repositories/users-repository";
+import { InvalidCredentialError } from "./errors/invalid-credential-error";
+import { compare } from "bcryptjs";
+import { User } from "@prisma/client";
+
+interface AuthenticateServiceRequest {
+    email: string
+    password: string
+}
+
+interface AuthenticateServiceResponse {
+    user: User
+}
+
+export class Authenticate {
+    constructor(
+        private usersRepository: UsersRepository
+    ) {}
+
+    async execute({ email, password }: AuthenticateServiceRequest): Promise<AuthenticateServiceResponse> {
+        const user = await this.usersRepository.findByEmail(email)
+
+        if (!user) {
+            throw new InvalidCredentialError()
+        }
+
+        const doesPasswordMatches = await compare(password, user.password_hash)
+
+        if (!doesPasswordMatches) {
+            throw new InvalidCredentialError()
+        }
+
+        return {
+            user
+        }
+    }
+}
--- src/services/register.test.ts
+++ src/services/register.test.ts
@@ -45,7 +45,7 @@ describe('Register Service', () => {
             password: '123456'
         })
 
-        expect(() => 
+        await expect(() => 
             registerService.handle({
                 name: 'Ricardo',
                 email,
