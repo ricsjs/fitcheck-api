51ca70b82ea63ccc5418bfc5bcbebbe4e37861dd
ricsjs <ricfilho0007@gmail.com>
2024-03-12 10:29:43 -0300

    feat: creation of check-in time validation

--- src/services/validate-check-in.test.ts
+++ src/services/validate-check-in.test.ts
@@ -12,11 +12,11 @@ describe('Validate Check-In Service', () => {
         checkInsRepository = new InMemoryCheckInsRepository
         sut = new CheckInValidateService(checkInsRepository)
 
-        // vi.useFakeTimers()
+        vi.useFakeTimers()
     })
 
     afterEach(() => {
-        // vi.useRealTimers()
+        vi.useRealTimers()
     })
 
     it('should be able to validate the check-in', async () => {
@@ -39,5 +39,24 @@ describe('Validate Check-In Service', () => {
                 checkInId: 'inexistent-check-in-id'
             })
         ).rejects.toBeInstanceOf(ResourceNotFoundError);
+    })
+
+    it('should not be able to validate the check-in after 20 minutes of its creation', async () => {
+        vi.setSystemTime(new Date(2024, 0, 1, 13, 40))
+
+        const createdCheckIn = await checkInsRepository.create({
+            gym_id: 'gym-01',
+            user_id: 'user-01'
+        })
+
+        const twentyOneMinutesInMs = 1000 * 60 * 21
+
+        vi.advanceTimersByTime(twentyOneMinutesInMs)
+
+        await expect(() =>
+            sut.execute({
+                checkInId: createdCheckIn.id
+            })
+        ).rejects.toBeInstanceOf(Error)
     })
 })
--- src/services/validate-check-in.ts
+++ src/services/validate-check-in.ts
@@ -4,6 +4,8 @@ import { ResourceNotFoundError } from "./errors/resource-not-found-errors";
 import { getDistanceBetweenCoordinates } from "@/utils/get-distance-between-coordinates";
 import { MaxNumberOfCheckInsError } from "./errors/max-number-of-check-ins-error";
 import { MaxDistanceError } from "./errors/max-distance-error";
+import dayjs from "dayjs";
+import { LateCheckInValidationError } from "./errors/late-check-in-validation-error";
 
 interface CheckInValidateServiceRequest {
     checkInId: string
@@ -23,6 +25,15 @@ export class CheckInValidateService {
 
         if (!checkIn) {
             throw new ResourceNotFoundError()
+        }
+
+        const distanceInMinutesFromCheckInCreation = dayjs(new Date()).diff(
+            checkIn.created_at,
+            'minutes'
+        )
+
+        if (distanceInMinutesFromCheckInCreation > 20) {
+            throw new LateCheckInValidationError()
         }
 
         checkIn.validated_at = new Date()
