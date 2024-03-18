2139af8bcb69057a3b03c3602d4d90b028df8e5a
ricsjs <ricfilho0007@gmail.com>
2024-03-18 09:56:44 -0300

    feat: test environment creation

---
+++ prisma/prisma-environment-prisma/prisma-test-environment.ts
@@ -0,0 +1,15 @@
+import { Environment } from "vitest"
+
+export default <Environment>{
+    name: 'prisma',
+    async setup() {
+        console.log('Setup')
+
+        return {
+            teardown() {
+                console.log('Teardown')
+            },
+        }
+    },
+    transformMode: 'ssr'
+}
