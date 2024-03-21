14da80874322d95273ef00211e430a866f3017cc
ricsjs <ricfilho0007@gmail.com>
2024-03-21 11:05:20 -0300

    feat: other gym controllers

--- src/http/controllers/users/gyms/routes.ts
+++ src/http/controllers/users/gyms/routes.ts
@@ -1,6 +1,13 @@
 import { verifyJWT } from "@/http/middlewares/verify-jwt"
 import { FastifyInstance } from "fastify"
+import { search } from "./search"
+import { nearby } from "./nearby"
+import { create } from "./create"
 
 export async function gymsRoutes(app: FastifyInstance) {
     app.addHook('onRequest', verifyJWT)
+
+    app.get('/gyms/search', search)
+    app.get('/gyms/nearby', nearby)
+    app.post('/gyms', create)
 }
---
+++ src/http/controllers/users/gyms/search.ts
@@ -0,0 +1,23 @@
+import { FastifyRequest, FastifyReply } from 'fastify'
+import { z } from "zod"
+import { makeSearchGymsService } from '@/services/factories/make-search-gyms-service'
+
+export async function search(request: FastifyRequest, reply: FastifyReply) {
+    const searchGymsQuerySchema = z.object({
+        gymName: z.string(),
+        page: z.coerce.number().min(1).default(1)
+    })
+
+    const { gymName, page } = searchGymsQuerySchema.parse(request.body)
+        
+        const searchGymsService = makeSearchGymsService()
+
+        const { gyms } = await searchGymsService.handle({
+            gymName,
+            page
+        })
+
+    return reply.status(201).send({
+        gyms
+    })
+}
